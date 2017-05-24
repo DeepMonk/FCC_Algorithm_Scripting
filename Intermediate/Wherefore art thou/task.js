892a80b3d0bdd2819fbbf4f193ab66f693b008ad
Alex Monk <deepmonk@mail.ru>
2017-05-24 22:00:46 +0300

    add  examples of using functions for task

--- Intermediate/Wherefore art thou/task.js
+++ Intermediate/Wherefore art thou/task.js
@@ -3,7 +3,11 @@ function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
-  
+  //alert (source.last); //Capulet
+  //alert (source.hasOwnProperty('last'));//true
+  //alert(Object.keys(source));//last
+  //alert(Object.keys(collection));//0, 1, 2 (или a, b (последний случай) т.к. "a" и "b")
+  //комбинируем
   
   // Only change code above this line
   return arr;
