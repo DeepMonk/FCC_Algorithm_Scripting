11314647f91de829504be24f79ecb3873a15b42f
Alex Monk <deepmonk@mail.ru>
2017-05-26 16:33:35 +0300

    add data search

--- Intermediate/Wherefore art thou/task.js
+++ Intermediate/Wherefore art thou/task.js
@@ -3,12 +3,21 @@ function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
-  //alert (source.last); //Capulet
-  //alert (source.hasOwnProperty('last'));//true
-  //alert(Object.keys(source));//last
-  //alert(Object.keys(collection));//0, 1, 2 (или a, b (последний случай) т.к. "a" и "b")
-  //комбинируем
-  
+  //значения все есть - составить логику.
+  //если соответствуют названия свойств:
+  alert(source.hasOwnProperty(Object.keys(collection[0])[0])); //соответствие названий свойств
+  //смотрим значения свойств если предыдущее true:
+  var name = Object.keys(collection[0])[0]; 
+  var prop = Object.keys(source)[0]; 
+  //alert(name);//first
+  //alert(prop); //last
+  //alert(collection[0][name]); //Romeo
+ // alert(source[prop]); //Capulet
+  if (source[prop]==collection[0][name])
+    {
+      alert ("ok");
+    }
+  else alert("not");
   // Only change code above this line
   return arr;
 }
