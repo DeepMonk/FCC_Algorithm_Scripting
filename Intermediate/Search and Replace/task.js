5024410a3636650f484618ad885079cb083014d0
Alex Monk <deepmonk@mail.ru>
2017-06-09 14:29:22 +0300

    new solution

--- Intermediate/Search and Replace/task.js
+++ Intermediate/Search and Replace/task.js
@@ -1,21 +1,11 @@
 
 function myReplace(str, before, after) {
-  //Использовать replace() и в качестве заменителя анонимную функцию
-  //заменяющую если надо первую букву.
-  if (before.charCodeAt(0)<=90)
-    {
-    
-  
-  var arrAfter=after.split(''); //работает
-     // after=arrAfter.splice(0, 1, after[0].toUpperCase()).join(''); //не работает
-      arrAfter.splice(0, 1, after[0].toUpperCase()); //работает
-      after=arrAfter.join(''); // работает
-      //after=after.split('').splice(0, 1, after[0].toUpperCase()).join('');
-      
-      //alert(after);
-      //after=after.join('');
-    }
-  return str.replace(before, after); //первая часть задачи - работает
+
+    return str.replace(before, function () {
+     var arrAfter=after.split('');
+     before.charCodeAt(0)<=90 ? arrAfter.splice(0, 1, after[0].toUpperCase()) : arrAfter.splice(0, 1, after[0].toLowerCase());
+      return arrAfter.join(''); 
+    });
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
